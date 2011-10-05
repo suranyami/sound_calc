c19c50d389c09543068f8eda7d4c9879e9f5f579
David Parry <david.parry@suranyami.com>
2011-10-05 23:08:22 +1100

    Calculating whole-number fractional harmonics from freq.

--- javascripts/sound_calc.coffee
+++ javascripts/sound_calc.coffee
@@ -4,15 +4,28 @@ class SoundCalc
   
   constructor: (@time = TIME_A) ->
   
-  frequency: (amount) ->
-    @time = 1.0 / amount
-
-  getFrequency: ->
-    1.0 / @time
-    
+  frequency: (amount) -> (@time = 1.0 / amount)
+  getFrequency: -> (@frequency_of @time)  
+  frequency_of: (time) -> (1.0 / time)
+  time_of: (freq) -> (1.0 / freq)
+  format_freq: (freq) -> "<span class='freq'>#{freq.toFixed(2)} Hz</span>"
+  format_time: (time) -> "<span class='time'>#{(time * 1000.0).toFixed(4) } ms</span>"
+  
+  harmonics: ->
+    frequency = @getFrequency()
+    result = for multiplier in [1..8]
+      (for divider in [1..8]
+        new_freq = frequency * multiplier / divider
+        h_freq = @format_freq(new_freq)
+        h_time = @format_time(@time_of(new_freq))
+        fraction = "<span class='fraction'>#{multiplier} / #{divider}:</span>"
+        "<div class='harm_row'>#{fraction} #{h_freq} #{h_time}</div>").join("\n")
+    result.join("\n")
+  
   update: ->
     $('#time').val(@time)
     $('#frequency').val(@getFrequency())
+    $('#harmonics').html(@harmonics())
 
 $(document).ready ->
   sound_calc = new SoundCalc
--- javascripts/sound_calc.js
+++ javascripts/sound_calc.js
@@ -11,11 +11,48 @@
       return this.time = 1.0 / amount;
     };
     SoundCalc.prototype.getFrequency = function() {
-      return 1.0 / this.time;
+      return this.frequency_of(this.time);
+    };
+    SoundCalc.prototype.frequency_of = function(time) {
+      return 1.0 / time;
+    };
+    SoundCalc.prototype.time_of = function(freq) {
+      return 1.0 / freq;
+    };
+    SoundCalc.prototype.format_freq = function(freq) {
+      return "<span class='freq'>" + (freq.toFixed(2)) + " Hz</span>";
+    };
+    SoundCalc.prototype.format_time = function(time) {
+      return "<span class='time'>" + ((time * 1000.0).toFixed(4)) + " ms</span>";
+    };
+    SoundCalc.prototype.harmonics = function() {
+      var divider, fraction, frequency, h_freq, h_time, multiplier, new_freq, result;
+      frequency = this.getFrequency();
+      result = (function() {
+        var _results;
+        _results = [];
+        for (multiplier = 1; multiplier <= 8; multiplier++) {
+          _results.push(((function() {
+            var _results2;
+            _results2 = [];
+            for (divider = 1; divider <= 8; divider++) {
+              new_freq = frequency * multiplier / divider;
+              h_freq = this.format_freq(new_freq);
+              h_time = this.format_time(this.time_of(new_freq));
+              fraction = "<span class='fraction'>" + multiplier + " / " + divider + ":</span>";
+              _results2.push("<div class='harm_row'>" + fraction + " " + h_freq + " " + h_time + "</div>");
+            }
+            return _results2;
+          }).call(this)).join("\n"));
+        }
+        return _results;
+      }).call(this);
+      return result.join("\n");
     };
     SoundCalc.prototype.update = function() {
       $('#time').val(this.time);
-      return $('#frequency').val(this.getFrequency());
+      $('#frequency').val(this.getFrequency());
+      return $('#harmonics').html(this.harmonics());
     };
     return SoundCalc;
   })();
